c25334e5011be36cec7258bde940a2bafa549c31
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-02 17:22:39 +0300

    Adding more code for additional combo charts

--- code/Combo chart/Combo chart.R
+++ code/Combo chart/Combo chart.R
@@ -10,7 +10,7 @@ superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dat
 superstore<-superstore%>%mutate(Order.Date=dmy(Order.Date))%>%
   mutate(mon = month(Order.Date, label = TRUE))
 
-#creating a combo chart (line + columns chart)
+#creating a combo chart (line + column chart)
 superstore%>%ggplot(aes(x = mon))+
   geom_bar(aes(y = Sales, fill = "Sales"), stat = "summary", fun = sum, width = 0.5)+
   geom_line(aes(y = Profit, color = "Profit",group = 1), stat = "summary", fun = sum, linewidth = 1)+
@@ -28,7 +28,7 @@ superstore%>%ggplot(aes(x = mon))+
         plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
         plot.caption = element_text(family = "serif", size = 9, color = "gray30"))
 
-#create combo chart (Line + stacked column)
+#create a combo chart (Line + stacked columns)
 superstore%>%ggplot(aes(x = mon))+
   geom_bar(aes(y = Sales, fill = Category), stat = "summary", fun = sum,position = "stack", width = 0.5, alpha = 0.8)+
   geom_line(aes(y = Profit, color = "Profit",group = 1), stat = "summary", fun = sum, linewidth = 1.1)+
@@ -47,7 +47,7 @@ superstore%>%ggplot(aes(x = mon))+
         plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
         plot.caption = element_text(family = "serif", size = 9, color = "gray30"))
 
-#create combo chart (line + clustered bars)
+#creating a combo chart (line + clustered columns)
 superstore%>%ggplot(aes(x = Region))+
   geom_bar(aes(y = Sales, fill = Category), stat = "summary", fun = sum,position = "dodge", width = 0.5, alpha = 0.8)+
   geom_line(aes(y = Profit, color = "Profit",group = 1), stat = "summary", fun = sum, linewidth = 1.5)+
@@ -63,5 +63,40 @@ superstore%>%ggplot(aes(x = Region))+
         axis.title = element_text(family = "serif", size = 10, color = "gray25"),
         legend.text = element_text(family = "serif", size = 9, color = "gray30"),
         legend.title = element_text(family = "serif", size = 10, color = "gray25"),
+        plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", size = 9, color = "gray30"))
+
+#creating a combo chart (area + column chart)
+superstore%>%ggplot(aes(x = mon))+
+  geom_area(aes(y = Profit, fill = "Profit",group = 1), stat = "summary", fun = sum)+
+  geom_bar(aes(y = Sales, fill = "Sales"), stat = "summary", fun = sum, width = 0.5)+
+  scale_fill_manual(values = c("Sales"="steelblue","Profit"="lightblue"))+
+  scale_y_continuous(labels = comma)+
+  labs(title = "Tracking Sales and Profit",
+       caption = "Viz by: Bernard Kilonzo",
+       x = "Month", y = "Value", fill = NULL, color = NULL)+
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "gray30"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray30"),
+        axis.title = element_text(family = "serif", size = 10, color = "gray25"),
+        legend.text = element_text(family = "serif", size = 9, color = "gray30"),
+        plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", size = 9, color = "gray30"))
+
+#creating a combo chart (line + area chart)
+superstore%>%ggplot(aes(x = mon))+
+  geom_area(aes(y = Sales, fill = "Sales", group = 1), stat = "summary", fun = sum)+
+  geom_line(aes(y = Profit, color = "Profit",group = 1), stat = "summary", fun = sum, linewidth = 1.2)+
+  scale_fill_manual(values = c("Sales"="lightblue"))+
+  scale_color_manual(values = c("Profit"="steelblue"))+
+  scale_y_continuous(labels = comma)+
+  labs(title = "Tracking Sales and Profit",
+       caption = "Viz by: Bernard Kilonzo",
+       x = "Month", y = "Value", fill = NULL, color = NULL)+
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "gray30"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray30"),
+        axis.title = element_text(family = "serif", size = 10, color = "gray25"),
+        legend.text = element_text(family = "serif", size = 9, color = "gray30"),
         plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
         plot.caption = element_text(family = "serif", size = 9, color = "gray30"))
