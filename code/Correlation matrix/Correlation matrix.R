0f1744ca66dfd361a218a1a56a040fe7aa4f6891
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-03 18:30:54 +0300

    updating the code and generating correlation plots

--- code/Correlation matrix/Correlation matrix.R
+++ code/Correlation matrix/Correlation matrix.R
@@ -8,7 +8,7 @@ sample_data<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/da
 corr_matrix<-cor(sample_data)
 
 #Creating correlation matrix (plot type full)
-ggcorrplot(corr_matrix, method = "square", type = "full",
+corr_v1<-ggcorrplot(corr_matrix, method = "square", type = "full",
            lab = TRUE, lab_size = 3.5, lab_col = "gray15",
            colors = c("brown","gray","steelblue"),
            legend.title = "correlation")+
@@ -19,9 +19,12 @@ ggcorrplot(corr_matrix, method = "square", type = "full",
         plot.title = element_text(family = "serif",face = "bold", size = 14, color = "gray25"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
 
+#saving the plot
+ggsave(plot = corr_v1, filename = "Corr_plot_v1.png",
+       width = 8, height = 6, units = "in", dpi = 300) 
 
 #creating correlation matrix_v2 (plot type = upper)
-ggcorrplot(corr_matrix, method = "square", type = "upper",#changing type to "upper"
+corr_v2<-ggcorrplot(corr_matrix, method = "square", type = "upper",#changing type to "upper"
            lab = TRUE, lab_size = 3.5, lab_col = "gray15",
            colors = c("brown","gray","steelblue"),
            legend.title = "correlation")+
@@ -32,9 +35,13 @@ ggcorrplot(corr_matrix, method = "square", type = "upper",#changing type to "upp
         legend.text = element_text(family = "serif", size = 8, colour = "gray35"),
         plot.title = element_text(family = "serif",face = "bold", size = 14, color = "gray25"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
+#saving the plot
+ggsave(plot = corr_v2, filename = "corr_plot_v2.png",
+       width = 8, height = 6, units = "in", dpi = 300) 
 
 #creating correlation matrix_v3 (plot type lower)
-ggcorrplot(corr_matrix, method = "square", type = "lower",#changing type to "lower"
+corr_v3<-ggcorrplot(corr_matrix, method = "square", type = "lower",#changing type to "lower"
            lab = TRUE, lab_size = 3.5, lab_col = "gray15",
            colors = c("brown","gray","steelblue"),
            legend.title = "correlation")+
@@ -46,8 +53,12 @@ ggcorrplot(corr_matrix, method = "square", type = "lower",#changing type to "low
         plot.title = element_text(family = "serif",face = "bold", size = 14, color = "gray25"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
 
+#saving the plot
+ggsave(plot = corr_v3, filename = "corr_plot_v3.png",
+       width = 8, height = 6, units = "in", dpi = 300) 
+
 #creating correlation matrix_v4 (plotting circle)
-ggcorrplot(corr_matrix, method = "circle", type = "lower",#method=circle & type=lower
+corr_v4<-ggcorrplot(corr_matrix, method = "circle", type = "lower",#method=circle & type=lower
            lab = TRUE, lab_size = 3, lab_col = "gray15",
            colors = c("brown","gray","steelblue"),
            legend.title = "correlation")+
@@ -58,3 +69,7 @@ ggcorrplot(corr_matrix, method = "circle", type = "lower",#method=circle & type=
         legend.text = element_text(family = "serif", size = 8, colour = "gray35"),
         plot.title = element_text(family = "serif",face = "bold", size = 14, color = "gray25"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
+#saving the plot
+ggsave(plot = corr_v4, filename = "corr_plot_v4.png",
+       width = 8, height = 6, units = "in", dpi = 300) 
