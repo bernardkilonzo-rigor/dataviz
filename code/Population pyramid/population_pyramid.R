ffde3996279f42a80baa2b02318febd51a8c5c82
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-11 17:33:15 +0300

    creating overlayed population pyramid chart

--- code/Population pyramid/population_pyramid.R
+++ code/Population pyramid/population_pyramid.R
@@ -39,5 +39,26 @@ bpp<-Ke_pop_v1%>%filter(Year==2019)%>%
 
 #saving the plot
 ggsave(plot = bpp, filename = "population_pyramid_1.png",
-       width = 8, height = 6, units = "in", dpi = 300) 
+       width = 8, height = 6, units = "in", dpi = 300)
 
+#Overlaying two periods (2009 & 2019)
+#creating grouped population pyramid plot
+Ke_pop_v1%>%filter(Year>2008)%>%
+  ggplot(aes(x = `Age Group`, y =Pop/1e6,fill =interaction(Gender, Year) ,group = Year))+
+  geom_bar(stat = "identity", position = position_dodge(width = 0.85), width = 1)+
+  coord_flip()+
+  scale_y_continuous(labels = abs)+
+  scale_fill_manual(values = c("Female.2019"="#618c03","Female.2009"="#bcd684","Male.2019"="#bfb304","Male.2009"="#d8d493"))+
+  labs(title = "Population Pyramid of Kenya (2009 & 2019)",
+       caption = "Viz by: Bernard Kilonzo",
+       x = "Age Group", y = "Population (millions)", fill ="")+
+  theme(panel.background = element_blank(),
+        panel.grid.major.y = element_line(color = "gray95", linewidth = 0.1),
+        panel.grid.major.x = element_line(color = "gray95",linetype = "dashed",linewidth = 0.1),
+        axis.ticks = element_line(colour = "gray88", linewidth = 0.1),
+        axis.title = element_text(family = "serif", size = 10,face = "bold", color = "gray30"),
+        axis.text = element_text(family = "serif", size = 8, color = "gray30"),
+        legend.text = element_text(family = "serif", size = 8, color = "gray30"),
+        legend.title = element_text(family = "serif",face = "bold" ,size = 9, color = "gray30"),
+        plot.title = element_text(family = "serif", face = "bold", size = 12, color = "gray25"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
