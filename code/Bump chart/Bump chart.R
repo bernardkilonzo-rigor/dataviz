eac5e19060204d15b88182d8549dca8bc1683a84
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-14 18:33:56 +0300

    creating a bump chart

---
+++ code/Bump chart/Bump chart.R
@@ -0,0 +1,35 @@
+#load libraries
+library(tidyverse)
+library(paletteer)
+
+#load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Sample%20-%20Superstore.csv")
+
+#computing rankings
+Rank_data<-superstore%>%
+  mutate(mon = month(Order.Date, label = TRUE))%>%
+  group_by(mon,Sub.Category)%>%
+  summarise(sales =sum(Sales))%>%
+  mutate(rank =dense_rank(desc(sales)))
+
+#creating bump chart
+Rank_data%>%ggplot(aes(x = mon, y = rank, group =Sub.Category, color =Sub.Category))+
+  geom_line(linewidth = 0.6)+
+  geom_point(size =2.5)+
+  scale_y_reverse()+
+  scale_fill_paletteer_d("palettetown::solrock")+
+  labs(title = "Bump Chart",
+       caption = "Viz by: Bernard Kilonzo",
+       x = "Months", y = "Performance Rankings")+
+  theme(panel.background = element_blank(),
+        axis.ticks = element_line(colour = "gray45"),
+        axis.title = element_text(family = "serif", size = 9, color = "gray35"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray35"),
+        legend.text = element_text(family = "serif", size = 9, color = "gray35"),
+        legend.position = "top",
+        legend.title = element_blank(),
+        plot.title = element_text(family = "serif", face = "bold", size = 12, colour = "gray25"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
+
+
