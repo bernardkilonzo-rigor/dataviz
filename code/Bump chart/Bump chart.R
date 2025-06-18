9114b2c79d90593cb1320680bbdf892bd4f488f5
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-18 16:58:35 +0300

    Updating code and chart

--- code/Bump chart/Bump chart.R
+++ code/Bump chart/Bump chart.R
@@ -12,24 +12,31 @@ Rank_data<-superstore%>%
   summarise(sales =sum(Sales))%>%
   mutate(rank =dense_rank(desc(sales)))
 
+#Adding labels
+Rank_data$label<-paste0(Rank_data$rank,"-",Rank_data$Region)
+
 #creating bump chart
 bc<-Rank_data%>%ggplot(aes(x = mon, y = rank, group =Region, color =Region))+
   geom_line(linewidth = 1.0)+
   geom_point(size =5)+
   scale_y_reverse()+
+  geom_text(data = Rank_data%>%filter(mon=="Jan"), aes(label = label), hjust=0.5,vjust=-1, size =3.5)+
+  geom_text(data = Rank_data%>%filter(mon=="Dec"), aes(label = label), hjust=0.5,vjust=-1, size =3.5)+
   scale_color_paletteer_d("PrettyCols::Autumn")+
-  labs(title = "Bump Chart",
+  labs(title = "Ranking Sales Performance by Region",
        caption = "Viz by: Bernard Kilonzo",
-       x = "Months", y = "Performance Rankings")+
+       x = "Months", y = "Rank")+
   theme(panel.background = element_blank(),
-        axis.ticks = element_line(colour = "gray45"),
-        axis.title = element_text(family = "serif", size = 9, color = "gray35"),
-        axis.text = element_text(family = "serif", size = 9, color = "gray35"),
-        legend.text = element_text(family = "serif", size = 9, color = "gray35"),
-        legend.position = "top",
-        legend.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(family = "serif", size = 9, color = "gray35"),
+        axis.text.x = element_text(family = "serif", size = 9, color = "gray35"),
+        legend.position = "none",
         plot.title = element_text(family = "serif", face = "bold", size = 12, colour = "gray25"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
 
 #Saving the plot
 ggsave(plot = bc, filename = "Bump_chart.png",
