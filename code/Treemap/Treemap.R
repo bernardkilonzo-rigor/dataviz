132e2d93b6d7235893f930ecc46e28925514848c
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-20 18:06:07 +0300

    updating treemap plot

--- code/Treemap/Treemap.R
+++ code/Treemap/Treemap.R
@@ -11,10 +11,12 @@ state_sales<-superstore%>%group_by(State)%>%
 
 #creating a tree map
 tmp<-state_sales%>%ggplot(aes(area = sales, fill = sales,label = State))+
-  geom_treemap(color = "gray45")+
-  scale_fill_gradient(low = "lightblue", high = "steelblue")+
+  geom_treemap(color = "gray85", size =2)+
+  scale_fill_gradient(low = "orange", high = "steelblue")+
   labs(title = "Revenue by State",
-       caption = "Viz by: Bernard Kilonzo", fill = "Legend")
+       caption = "Viz by: Bernard Kilonzo", fill = "Sales")+
+  theme(plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
 
 
 #save 100% stacked bar plot
