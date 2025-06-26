82a7dd161bc8ea6d01e4cb90f7c535e798fc3742
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-06-26 17:19:06 +0300

    revisisng slope chart

--- code/Slope chart/Slope chart.R
+++ code/Slope chart/Slope chart.R
@@ -6,26 +6,27 @@ library(paletteer)
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Sample%20-%20Superstore.csv")
 
 #computing years from order date
-superstore<-superstore%>%mutate(Order.Date=dmy(Order.Date))%>%
+superstore<-superstore%>% mutate(Order.Date=dmy(Order.Date))%>%
   mutate(yr = year(Order.Date))
 
-#computing ranks by yr & state
-Rank_data<-superstore%>%filter(yr>2018)%>%
-  group_by(yr,Sub.Category)%>%
+#computing ranks by yr & state in east region
+Rank_data<-superstore%>%filter(yr>2018, Region=="East")%>%
+  group_by(yr,State)%>%
   summarise(sales =round(sum(Sales),0))%>%
   mutate(rank =dense_rank(desc(sales)))
 
 #computing labels
-Rank_data$label<-paste0(Rank_data$rank,"-",Rank_data$Sub.Category,"~",Rank_data$sales)
+Rank_data$label<-paste0(Rank_data$rank,"-",Rank_data$State,"~",Rank_data$sales)
 
 #creating slope chart
-sc<-Rank_data%>%ggplot(aes(x = yr, y = rank, group =Sub.Category, color =Sub.Category))+
+Rank_data%>%ggplot(aes(x = yr, y = rank, group =State, color =State))+
   geom_line(linewidth = 1.0)+
   geom_point(size =4)+
   scale_y_reverse()+
+  scale_color_paletteer_d("palettetown::combusken")+
   geom_text(data = Rank_data%>%filter(yr==2019), aes(label = label), hjust =0.1, vjust=-0.7, size =3)+
   geom_text(data = Rank_data%>%filter(yr==2020), aes(label = label), hjust =0.8, vjust=-0.7, size =3)+
-  labs(title = "Ranking Sales by Sub-Category",
+  labs(title = "Ranking Sales by State in East Region",
        caption = "Viz by: Bernard Kilonzo",
        x = "Year", y = "Rank")+
   theme(panel.background = element_blank(),
