6c4d45b126dd8b5e4a5efdf5fdd58824780f3402
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-03 16:55:24 +0300

    formatting the heatmap plot

--- code/Heatmap/Heatmap.R
+++ code/Heatmap/Heatmap.R
@@ -14,4 +14,14 @@ profit_summary<-superstore%>%mutate(mon = month(Order.Date, label=TRUE))%>%
 #creating heat map
 profit_summary%>%ggplot(aes(x = mon, y = Sub.Category, fill = profit))+
   geom_tile(color = "white", lwd =0.1)+
-  scale_fill_gradient2(low = "red", mid = "gray90", high = "green", midpoint = 0)
+  scale_fill_gradient2(low = "red", mid = "gray90", high = "green", midpoint = 0)+
+    labs(title = "Analysis of Profit by Month & Sub Category",
+       x = "Month", y = "Sub Category", caption = "Viz  by: Bernard Kilonzo",
+       fill = "Profit")+
+  theme(panel.background = element_blank(),
+        axis.title = element_text(family = "serif", face = "bold", size = 10, color = "gray30"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray35"),
+        legend.title = element_text(family = "serif",face = "bold", size = 9, color = "gray30"),
+        legend.text = element_text(family = "serif", color = "gray30", size = 9),
+        plot.title = element_text(family = "serif", face = "bold", size = 13,color = "gray25"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
