3692dce1ee3eb3aae06c553b7e2820744f30b294
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-05 16:38:32 +0300

    Updating color palette

--- code/Treemap/Treemap.R
+++ code/Treemap/Treemap.R
@@ -1,6 +1,7 @@
 #load libraries
 library(tidyverse)
 library(treemapify)
+library(paletteer)
 
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Sample%20-%20Superstore.csv")
@@ -11,14 +12,14 @@ state_sales<-superstore%>%group_by(State)%>%
 
 #creating a tree map
 tmp<-state_sales%>%ggplot(aes(area = sales, fill = sales,label = State))+
-  geom_treemap(color = "gray85", size =2)+
-  scale_fill_gradient(low = "orange", high = "steelblue")+
+  geom_treemap(color = "gray85", size =1)+
+  scale_fill_paletteer_c("grDevices::RdYlBu")+
   labs(title = "Revenue by State",
        caption = "Viz by: Bernard Kilonzo", fill = "Sales")+
   theme(plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
 
-
+tmp
 #save 100% stacked bar plot
 ggsave(plot = tmp, filename = "Treemap.png",
        width = 8, height = 6, units = "in", dpi = 300) 
