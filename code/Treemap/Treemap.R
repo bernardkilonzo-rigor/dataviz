d8c6598f3ee4f33c8c25dd5cdd5abb5b74f0e634
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-05 16:48:36 +0300

    updating treemap

--- code/Treemap/Treemap.R
+++ code/Treemap/Treemap.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(treemapify)
 library(paletteer)
+library(scales)
 
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Sample%20-%20Superstore.csv")
@@ -19,7 +20,6 @@ tmp<-state_sales%>%ggplot(aes(area = sales, fill = sales,label = State))+
   theme(plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
 
-tmp
 #save 100% stacked bar plot
 ggsave(plot = tmp, filename = "Treemap.png",
        width = 8, height = 6, units = "in", dpi = 300) 
