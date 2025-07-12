69aa84112c78591769c3cf1536bcadf45420b901
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-12 13:29:48 +0300

    updating the code and saving the plots

--- code/Boxplot & Violin Plot/Boxplot & Violin Plot.R
+++ code/Boxplot & Violin Plot/Boxplot & Violin Plot.R
@@ -1,4 +1,3 @@
-setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Boxplot & Violin Plot")
 #load libraries
 library(tidyverse)
 library(paletteer)
@@ -7,7 +6,7 @@ library(paletteer)
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
 #create violin plot with boxplot
-superstore%>%ggplot(aes(x =Region, y =Quantity, color =Region))+
+vbp<-superstore%>%ggplot(aes(x =Region, y =Quantity, color =Region))+
   geom_violin()+
   geom_boxplot(width =0.07)+
   scale_color_paletteer_d("rockthemes::faithnomore")+
@@ -23,9 +22,12 @@ superstore%>%ggplot(aes(x =Region, y =Quantity, color =Region))+
         plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
 
+#saving the plot
+ggsave(plot = vbp, filename = "boxplot_violin_plot_1.png",
+       width = 8, height = 6, units = "in", dpi = 300)
 
 ##create violin plot with boxplot (filling the plot)
-superstore%>%ggplot(aes(x =Region, y =Quantity, fill =Region))+
+vbp_2<-superstore%>%ggplot(aes(x =Region, y =Quantity, fill =Region))+
   geom_violin()+
   geom_boxplot(width =0.07)+
   scale_fill_paletteer_d("wesanderson::Chevalier1")+
@@ -40,3 +42,7 @@ superstore%>%ggplot(aes(x =Region, y =Quantity, fill =Region))+
         legend.text = element_text(family = "serif", size = 9, color = "gray35"),
         plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
+
+#saving the plot
+ggsave(plot = vbp_2, filename = "boxplot_violin_plot_2.png",
+       width = 8, height = 6, units = "in", dpi = 300)
