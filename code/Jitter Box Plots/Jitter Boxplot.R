0188d0eb9c10e55338c0aecb8aada00a32125f11
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-14 16:01:20 +0300

    cleaning the plot

--- code/Jitter Box Plots/Jitter Boxplot.R
+++ code/Jitter Box Plots/Jitter Boxplot.R
@@ -11,4 +11,16 @@ superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dat
 superstore%>%filter(Region=="East")%>%ggplot(aes(y = State, x = Quantity, color = State))+
   stat_boxplot(geom = "errorbar",width =0.4)+
   geom_boxplot(outlier.color = NA)+#removing the outliers
-  geom_jitter(size =0.7)
+  geom_jitter(size =0.7)+
+  scale_color_paletteer_d("impressionist.colors::irissen")+
+  labs(title = "Order Distribution by Quantity by State",
+       caption = "Viz by: Bernard Kilonzo")+
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "gray30", linewidth = 0.2),
+        axis.ticks = element_line(color = "gray30", linewidth = 0.2),
+        axis.title = element_text(family = "serif", size = 10,face = "bold", color = "gray30"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray30"),
+        legend.position = "none",
+        plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray25"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
