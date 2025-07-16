25098cb5d1e40b6e05262ac84a0bc26cddacd5c6
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-16 11:18:44 +0300

    updating the code to save the plot

--- code/Histogram/Histogram.R
+++ code/Histogram/Histogram.R
@@ -23,8 +23,19 @@ ggsave(plot = hp, filename = "Histogram_plot.png",
        width = 8, height = 6, units = "in", dpi = 300)
 
 #histogram with density
-superstore%>%filter(Sales<500)%>%ggplot(aes(x = Sales))+
+hdp<-superstore%>%filter(Sales<500)%>%ggplot(aes(x = Sales))+
   geom_histogram(aes(y = after_stat(density)),binwidth = 20, color="gray2",linewidth =0.2, fill ="white")+
-  geom_density(linewidth =0.5, color ="brown", fill = "gold", alpha =0.5)+
+  geom_density(linewidth = 0.8, color ="steelblue", fill = "lightblue", alpha =0.5)+
   scale_x_continuous(limits = c(0,NA), expand = c(0,0))+
-  scale_y_continuous(limits = c(0,NA), expand = c(0,0))
+  scale_y_continuous(limits = c(0,NA), expand = c(0,0))+
+  labs(title = "Histogram with Density Plot", caption = "Viz by Bernard Kilonzo")+
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "gray30", linewidth = 0.2),
+        axis.title = element_text(family = "serif", face = "bold",color = "gray30", size = 10),
+        axis.text = element_text(family = "serif", color = "gray30", size = 9),
+        plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray25"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
+#saving the plot
+ggsave(plot = hdp, filename = "histogram_density_plot.png",
+       width = 8, height = 6, units = "in", dpi = 300)
