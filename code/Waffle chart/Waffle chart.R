83bca372c6b7bbf5893ee70c0ab963ad955008d3
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-17 12:16:14 +0300

    cleaning the code and saving the plot

--- code/Waffle chart/Waffle chart.R
+++ code/Waffle chart/Waffle chart.R
@@ -1,4 +1,3 @@
-setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Waffle chart")
 #load libraries
 library(tidyverse)
 library(waffle)
@@ -14,13 +13,19 @@ sales_prop<-superstore%>%group_by(Ship.Mode)%>%
   mutate(percnt = round(proprtion*100,0))
 
 #creating waffle chart
-sales_prop%>%ggplot(aes(fill =Ship.Mode, values =percnt))+
+wfc<-sales_prop%>%ggplot(aes(fill =Ship.Mode, values =percnt))+
   geom_waffle(n_rows = 10, color ="white")+
   scale_fill_paletteer_d("rockthemes::electric")+
   labs(title = "Waffle Chart - Proportion of Sales by Shipping Mode",
        caption = "Viz by: Bernard Kilonzo", fill ="Ship Mode")+
-  theme_void()+
-  theme(plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray30"),
+  theme(panel.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray30"),
         plot.caption = element_text(family = "serif", face = "italic",size = 9, color = "gray35"),
         legend.title = element_text(family = "serif",face = "bold", size = 10, color = "gray30"),
         legend.text = element_text(family = "serif", size = 9, color = "gray30"))
+
+#saving the plot
+ggsave(plot = wfc, filename = "waffle_chart.png",
+       width = 8, height = 6, units = "in", dpi = 300)
