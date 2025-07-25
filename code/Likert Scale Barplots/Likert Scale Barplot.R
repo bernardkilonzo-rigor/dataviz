fca026932dffbcdde10f0cf93bea140f50a0e46e
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-25 12:52:52 +0300

    Initial commit

--- code/Likert Scale Barplots/Likert Scale Barplot.R
+++ code/Likert Scale Barplots/Likert Scale Barplot.R
@@ -15,18 +15,15 @@ q4[]<-lapply(q4, factor, levels =c("Highly dissatisfied","Dissatisfied",
 #using likert() to summarize likert items across questions
 likert_summary<-likert(q4)
 
-likert_summary
+
 
 #create a plot
 plot(likert_summary)+
   scale_fill_manual(values = c("#bf623d", "#e3a462", "#d4d4d4", "#a7cee8", "#6b9bc2"))
 
-ggsave(plot = x, filename = "Image 3.png",
-       width = 8, height = 6, units = "in", dpi = 300)
+
 
 y<-plot(likert_summary)+
 scale_fill_manual(values = c("#bf623d", "#e3a462", "#d4d4d4", "#a7cee8", "#6b9bc2"))
 
 
-ggsave(plot = y, filename = "Image 4.png",
-       width = 8, height = 6, units = "in", dpi = 300)
