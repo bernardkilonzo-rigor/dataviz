3ef4f1606efff4d4d698305fce4bea577e841fa2
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-28 12:42:18 +0300

    Customizing/editing colors

--- code/Likert Scale Barplots/Likert Scale Barplot.R
+++ code/Likert Scale Barplots/Likert Scale Barplot.R
@@ -27,4 +27,11 @@ lsp<-plot(likert_summary)+
 ggsave(plot = lsp, filename = "Likert_scale_plot.png",
        width = 8, height = 6, units = "in", dpi = 300)
 
+#customizing  (editing) colors
+lsp_1<-plot(likert_summary)+
+  scale_fill_manual(values = c("#bf623d", "#e3a462", "#d4d4d4", "#a7cee8", "#6b9bc2"))+
+  labs(title = "Level of Satisfaction",
+       caption = "Viz by: Bernard  Kilonzo")+
+  theme(plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray30"))
 
