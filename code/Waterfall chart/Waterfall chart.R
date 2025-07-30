01d5c9218b26ec95e91c6077483d4137458db998
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-07-30 20:35:35 +0300

    formatting the plot with theme

--- code/Waterfall chart/Waterfall chart.R
+++ code/Waterfall chart/Waterfall chart.R
@@ -15,6 +15,15 @@ waterfall(profit_summary,
           draw_lines = FALSE,#remove lines
           rect_width = 0.8,#rectangles width
           rect_text_size = 0.8,
-          total_rect_color = "brown",
+          total_rect_color = "steelblue",
           calc_total = TRUE)+
-  theme_minimal()
+  theme_minimal()+
+  labs(title = "Waterfall Chart",
+       caption = "Viz by: Bernard Kilonzo",
+       x ="Sub_Category", y = "Cummulative Profit")+
+  theme(panel.grid = element_blank(),
+        axis.line = element_line(color = "gray30", linewidth = 0.1),
+        axis.title = element_text(family = "serif",face = "bold", color = "gray25", size = 10),
+        axis.text = element_text(family = "serif", color = "gray35", size = 9),
+        plot.title = element_text(family = "serif", face = "bold", color = "gray20", size = 13),
+        plot.caption = element_text(family = "serif", face = "italic", color = "gray40", size = 9))
