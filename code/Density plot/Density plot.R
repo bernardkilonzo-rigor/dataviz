2220a9fef403a981d32960ce0a6896de9c8ccdff
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-01 15:02:28 +0300

    Creating similar density plots without filling  the density areas

--- code/Density plot/Density plot.R
+++ code/Density plot/Density plot.R
@@ -6,7 +6,12 @@ library(tidyverse)
 #load data
 survey_data<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Survey%20Data.csv")
 
-#Simple density plot
+#Simple density plot (option 1)
 survey_data%>%ggplot(aes(x = Q6d, fill = Gender))+
   geom_density(alpha =0.4)+
   scale_fill_manual(values = c("maroon", "steelblue"))
+
+#simple density plot (option 2)
+survey_data%>%ggplot(aes(x = Q6d, color = Gender))+
+  geom_density(lwd =1)+
+  scale_color_manual(values = c("maroon", "steelblue"))
