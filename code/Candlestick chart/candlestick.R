7da68d44f20381394b4ffa38446186d0d61e2db9
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-06 12:31:33 +0300

    updating the code to save the first candlestick chart

--- code/Candlestick chart/candlestick.R
+++ code/Candlestick chart/candlestick.R
@@ -7,7 +7,7 @@ library(xts)
 #create a candlestick using local data
 #load data
 stock<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Stock%20Exchange%20Sample%20Data.csv")
-
+view(stock)
 #converting date to a date object
 stock$date <- as.Date(stock$date, format = "%d/%m/%Y")
 
@@ -18,13 +18,19 @@ new_stock<-stock%>%select(-symbol)
 new_stock_xts<-xts(new_stock[,-1], order.by = new_stock$date)
 
 #creating a simple plot
-chartSeries(new_stock_xts, type = "candlesticks", theme = chartTheme("white"))
+chartSeries(new_stock_xts, type = "candlesticks",
+            theme = chartTheme("white"),
+            name = "WLTW Stock")
 
-#correct the formatting of the date field and finalized the chart.
-
+#saving the plot
+png("candlestick_chart.png", width = 800, height = 600) #opens PNG graphics device
+chartSeries(new_stock_xts, type = "candlesticks",
+            theme = chartTheme("white"),
+            name = "WLTW Stock")
+dev.off() #Closes the graphics device.
 
 #create a candlestick using real-time data
-#load data
+#load data (getting Google stock data)
 getSymbols("GOOG", src = "yahoo", from = "2024-07-01", to = "2025-06-30")
 
 #create the plot
