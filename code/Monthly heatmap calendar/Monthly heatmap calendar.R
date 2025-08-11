5dbf4f4b19cdb1b4223d49f518917b20be8cd933
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-11 15:14:42 +0300

    preparing data and creating monthly heatmap calendar

--- code/Monthly heatmap calendar/Monthly heatmap calendar.R
+++ code/Monthly heatmap calendar/Monthly heatmap calendar.R
@@ -9,3 +9,16 @@ superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dat
 superstore$Order.Date<-as.Date(superstore$Order.Date, format = "%d/%m/%Y")
 
 #computing the required data
+calendar_data<-superstore%>%
+  filter(year(Order.Date)==2020, month(Order.Date)==12)%>%
+  mutate(month = month(Order.Date, label = TRUE),
+  weekday =wday(Order.Date, label = TRUE),
+  day = day(Order.Date),
+  week = isoweek(Order.Date))
+
+#creating monthly heatmap calendar
+calendar_data%>%ggplot(aes(x = weekday, y = week, fill = Sales))+
+  geom_tile(color = "white")+
+  geom_text(aes(label = day), size =3, color = "black")+
+  scale_fill_distiller(palette = "YlOrRd", direction = 1)
+
