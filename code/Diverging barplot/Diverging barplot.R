d362e0e59e37b8c12228416907f08667db8abc87
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-14 13:41:13 +0300

    computing new summary figures and classification based on performance

--- code/Diverging barplot/Diverging barplot.R
+++ code/Diverging barplot/Diverging barplot.R
@@ -6,8 +6,19 @@ library(tidyverse)
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 superstore$Order.Date<-as.Date(superstore$Order.Date, format = "%d/%m/%Y")
 
+#computing profit by sub-category in the central region
+profitability<-superstore%>%filter(Region=="Central")%>%
+  group_by(Sub.Category)%>%
+  summarise(profit =sum(Profit))
+
+#classifying data set
+profitability$type<-ifelse(profitability$profit >= 0, "Profitable", "Unprofitable")
+
+
+
+
 #Summarizing performance by month for the two years
-superstore%>%mutate(mon = month(Order.Date, label = TRUE))%>%
+summary_metrics<-superstore%>%mutate(mon = month(Order.Date, label = TRUE))%>%
   group_by(mon)%>%
   summarise(sales_2019 =sum(ifelse(year(Order.Date)==2019, Sales,0)),
             sales_2020 =sum(ifelse(year(Order.Date)==2020, Sales,0)))
