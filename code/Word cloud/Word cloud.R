3a6eece430802d3efb10fdeb42d0f98cc9758d84
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-21 12:49:53 +0300

    Initial commit, word cloud chart

---
+++ code/Word cloud/Word cloud.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Word cloud")
+
+#Load libraries
+library(tm) #text mining
+library(wordcloud) #word cloud generation
+library(RColorBrewer) #color palettes
+
+#load data set
+
