35815738be7165a2d1af7c63a5f69f83d380e40a
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-01 16:08:41 +0300

    Initial commit on how to create a stepped line chart

---
+++ code/Stepped Line Chart/Stepped line chart.R
@@ -0,0 +1,6 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Stepped Line Chart")
+#load libraries
+library(ggplot2)
+
+#load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
