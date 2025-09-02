2fac96a795235852d0a4dc138f1b6108785abc30
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-02 15:18:40 +0300

    de-cluttering the stepped line chart

--- code/Stepped Line Chart/Stepped line chart.R
+++ code/Stepped Line Chart/Stepped line chart.R
@@ -14,4 +14,13 @@ sales_summary<-superstore%>%mutate(Order.Date = dmy(Order.Date))%>%
 
 #creating a simple stepped line chart
 sales_summary%>%ggplot(aes(x = my, y = sales))+
-  geom_step(color ="steelblue", linewidth =1.1)
+  geom_step(color ="brown", linewidth =1.1)+
+  labs(title = "Revenue by Month",
+       x = "Month", y = "Revenue", caption = "Viz by: Bernard Kilonzo")+
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "gray30", linewidth = 0.3),
+        axis.ticks = element_line(color = "gray30", linewidth = 0.3),
+        axis.title = element_text(family = "serif", face = "bold", size = 10, color = "gray25"),
+        axis.text = element_text(family = "serif", color = "gray35", size = 9),
+        plot.title = element_text(family = "serif", face = "bold",size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
