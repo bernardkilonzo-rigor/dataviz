871d3e8b3b1923e303bec8ea4eb57296adeae6f5
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-03 11:41:03 +0300

    Computing the cummulative sales and percentage

--- code/Pareto Chart/Pareto chart.R
+++ code/Pareto Chart/Pareto chart.R
@@ -10,3 +10,12 @@ superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dat
 state_sales<-superstore%>%
   group_by(State)%>%
   summarise(sales =sum(Sales))
+
+#Sorting and computing cumulative percentage
+state_sales<-state_sales%>%
+  arrange(desc(sales))%>%
+  mutate(cum_sales =cumsum(sales),
+         cum_perc = cum_sales/sum(sales)*100)
+
+#creating pareto chart in ggplot2
+
