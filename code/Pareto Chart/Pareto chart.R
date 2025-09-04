7047a1c2bd0ccd190a24a2f95dd26e20d212cb3b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-04 16:47:19 +0300

    fomarting plot text elements

--- code/Pareto Chart/Pareto chart.R
+++ code/Pareto Chart/Pareto chart.R
@@ -21,8 +21,8 @@ state_sales<-state_sales%>%
 #creating a Pareto chart in ggplot2
 state_sales%>%ggplot(aes(x = reorder(State, -sales)))+
   geom_bar(aes(y = sales), stat = "identity", fill = "steelblue")+
-  geom_line(aes(y = cum_perc * max(sales) / 100, group = 1), color ="red", linewidth =1)+
-  geom_point(aes(y = cum_perc * max(sales)/100), color = "red", size =2)+
+  geom_line(aes(y = cum_perc * max(sales) / 100, group = 1), color ="red", linewidth =0.7)+
+  geom_point(aes(y = cum_perc * max(sales)/100), color = "red", size =1)+
   scale_y_continuous(
     name = "Sales",
     sec.axis = sec_axis(~ . * 100 / max(state_sales$sales), name = "Cumulative Percentage"
@@ -31,4 +31,8 @@ state_sales%>%ggplot(aes(x = reorder(State, -sales)))+
        x = "State", caption = "Viz By: Bernard Kilonzo")+
   theme(panel.background = element_blank(),
         axis.line = element_line(color = "gray30", linewidth = 0.1),
-        axis.ticks = element_line(color = "gray30", linewidth = 0.1))
+        axis.ticks = element_line(color = "gray30", linewidth = 0.1),
+        axis.title = element_text(family = "serif", face = "bold", size = 10, color = "gray25"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray30"),
+        plot.title = element_text(family = "serif", face = "bold", size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray40"))
