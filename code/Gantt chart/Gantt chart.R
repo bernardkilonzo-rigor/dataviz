7ad4d912c81043488251970c0dc9fa7491debc4c
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-06 12:50:52 +0300

    combining expert assigned and their task

--- code/Gantt chart/Gantt chart.R
+++ code/Gantt chart/Gantt chart.R
@@ -4,12 +4,17 @@ library(tidyverse)
 
 #load data
 project_timeline<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Project%20Timelines.csv")
-
+view(project_timeline)
 #converting start and end dates into date objects
 project_timeline$Start.date<-dmy(project_timeline$Start.date)
 project_timeline$End.date<-dmy(project_timeline$End.date)
 
+#combining expert assigned and the tasks
+project_timeline<-project_timeline%>%
+  mutate(label = paste(Expert.Assigned, Task, sep = "-"))
+
 #creating a basic gantt chart
 project_timeline%>%ggplot(aes(x =Start.date, xend =End.date, y = Expert.Assigned,
                               yend = Expert.Assigned, color = Task))+
+  facet_grid(~Task, scales = "free_y")+
   geom_segment(linewidth =8)
