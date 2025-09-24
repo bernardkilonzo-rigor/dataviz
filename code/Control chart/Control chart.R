f0830085327423df06761202f1e1147ca16ca7a2
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-24 18:17:43 +0300

    loading dataset

--- code/Control chart/Control chart.R
+++ code/Control chart/Control chart.R
@@ -4,3 +4,15 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Control c
 library(tidyverse)
 
 #load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
+
+
+
+#create control chart
+superstore%>%ggplot(aes(x = , y = Value)) +
+  geom_point() + 
+  geom_line() +
+  stat_QC(method = "XmR", auto.label = TRUE, label.digits = 2, show.1n2.sigma = TRUE) +
+  stat_QC(method = "mR") +
+  scale_x_continuous(expand = expansion(mult = 0.15))
