5b5a9a451c7f732a571894fd9d8b03a9ea79503f
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-24 18:20:37 +0300

    extracting month year and grouping sales by month year

--- code/Control chart/Control chart.R
+++ code/Control chart/Control chart.R
@@ -2,15 +2,21 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Control c
 
 #load libraries
 library(tidyverse)
+library(zoo)
 
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
+#extracting month year from order date
+superstore<-superstore%>%mutate(Order.Date = dmy(Order.Date))%>%
+  mutate(my = as.yearmon(Order.Date))
 
-
+#computing sales by month year
+super_cal<-superstore%>%group_by(my)%>%
+  summarise(sales =sum(Sales))
 
 #create control chart
-superstore%>%ggplot(aes(x = , y = Value)) +
+superstore%>%ggplot(aes(x = my, y = Value)) +
   geom_point() + 
   geom_line() +
   stat_QC(method = "XmR", auto.label = TRUE, label.digits = 2, show.1n2.sigma = TRUE) +
