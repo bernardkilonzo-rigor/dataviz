b2dcfeda5d16b3e232d181fe551a3e13a363388a
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-27 12:34:56 +0300

    decluttering the code

--- code/Control chart/Control chart.R
+++ code/Control chart/Control chart.R
@@ -7,26 +7,26 @@ library(qicharts2)
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
-#extracting month year from order date
+#extracting weeks from order date
 superstore<-superstore%>%mutate(Order.Date = dmy(Order.Date))%>%
   mutate(week = week(Order.Date))
 
-#computing sales by month year
+#computing sales by week, cl, ucl and lcl
 super_cal<-superstore%>%group_by(week)%>%
   summarise(sales =sum(Sales))%>%
-  mutate(mean = mean(sales),
+  mutate(cl = mean(sales),
             sd = sd(sales),
             ucl = mean(sales)+2*sd,
             lcl = mean(sales)-2*sd)
 
-#create control chart
+#creating control chart
 qc<-qic(sales,data = super_cal, chart="xbar")
-qc
+
 
 #formatting
 qc +
   geom_hline(yintercept = super_cal$ucl, linetype = "dashed", color = "red") +
   geom_hline(yintercept = super_cal$lcl, linetype = "dashed", color = "blue") +
-  geom_hline(yintercept = super_cal$mean,  linetype = "solid",  color = "black") +
+  geom_hline(yintercept = super_cal$cl,  linetype = "solid",  color = "black") +
   labs(title = "Control Chart with Manual Limits")
 
