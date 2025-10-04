51ea5c230661bf978d712841101ccce5474001f8
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-04 16:23:59 +0300

    creating KPI gauge chart

--- code/Gauge chart/Gauge chart.R
+++ code/Gauge chart/Gauge chart.R
@@ -3,3 +3,24 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Gauge cha
 #load libraries
 library(plotly)
 
+#creating gauge chart
+plot_ly(
+  type = "indicator",
+  mode = "gauge+number+delta",
+  value = 70,                        #Current value
+  delta = list(reference = 80),      #Target value for comparison
+  gauge = list(
+    axis = list(range = list(0, 100)), #Axis range
+    steps = list(
+      list(range = c(0, 50), color = "lightgray"),
+      list(range = c(50, 80), color = "yellow"),
+      list(range = c(80, 100), color = "green")
+    ),
+    threshold = list(
+      line = list(color = "red", width = 4),
+      thickness = 0.75,
+      value = 90                       # Threshold marker
+    )
+  ),
+  title = list(text = "KPI Gauge Chart")
+)
