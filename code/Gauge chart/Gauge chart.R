d8a52069dbe0c6b4c5a4a3b1502bc3c147b08b69
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-04 16:30:08 +0300

    updating code details

--- code/Gauge chart/Gauge chart.R
+++ code/Gauge chart/Gauge chart.R
@@ -7,19 +7,19 @@ library(plotly)
 plot_ly(
   type = "indicator",
   mode = "gauge+number+delta",
-  value = 70,                        #Current value
-  delta = list(reference = 80),      #Target value for comparison
+  value = 55,                        #Current value
+  delta = list(reference = 85),      #Target value for comparison
   gauge = list(
     axis = list(range = list(0, 100)), #Axis range
     steps = list(
-      list(range = c(0, 50), color = "lightgray"),
-      list(range = c(50, 80), color = "yellow"),
-      list(range = c(80, 100), color = "green")
+      list(range = c(0, 35), color = "lightgray"),
+      list(range = c(35, 70), color = "yellow"),
+      list(range = c(70, 100), color = "green")
     ),
     threshold = list(
       line = list(color = "red", width = 4),
       thickness = 0.75,
-      value = 90                       # Threshold marker
+      value = 80                       # Threshold marker
     )
   ),
   title = list(text = "KPI Gauge Chart")
