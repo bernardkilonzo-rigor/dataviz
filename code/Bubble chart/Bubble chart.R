3fb8bc38cb3a4fba543b78ecafb866d5fd50b63f
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-13 21:07:04 +0300

    Initial commit, creating bubble chart in ggplot2

---
+++ code/Bubble chart/Bubble chart.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Bubble chart")
+
+#load libraries
+library(tidyverse)
+
+#load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
