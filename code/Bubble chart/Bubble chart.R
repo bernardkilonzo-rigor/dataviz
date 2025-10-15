8bd5f3d912381de69ce5e561ddfafecb8632dd6d
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-15 17:08:33 +0300

    creating sample bubble plot in ggplot2

--- code/Bubble chart/Bubble chart.R
+++ code/Bubble chart/Bubble chart.R
@@ -6,3 +6,7 @@ library(tidyverse)
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
+#creating bubble chart
+superstore%>%ggplot(aes(x = Sales, y = Profit, size = Discount, color = Profit))+
+  geom_point(alpha = 0.5)+
+  labs(title = "Bubble Plot")
