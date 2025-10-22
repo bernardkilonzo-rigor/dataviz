d7011a56598497a98e5fd63232c7294aea903127
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-22 17:10:29 +0300

    Decluttering the chart by applying theme

--- code/Marimekko Chart/Marimekko chart.R
+++ code/Marimekko Chart/Marimekko chart.R
@@ -4,24 +4,22 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Marimekko
 install.packages("ggmosaic")
 library(ggmosaic)
 library(tidyverse)
+library(paletteer)
 
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
-
-#create sample data
-data <- data.frame(
-  Gender = c("Male", "Male", "Female", "Female"),
-  Product = c("A", "B", "A", "B"),
-  Count = c(40, 60, 70, 30)
-)
-
-#convert to frequency table
-data_expanded <- data %>%
-  uncount(weights = Count)
-view(data_expanded)
-
 #create marimekko chart
-ggplot(data_expanded) +
-  geom_mosaic(aes(x = product(Gender, Product), fill = Product))+
-  theme_minimal()
+ggplot(superstore) +
+  geom_mosaic(aes(x = product(Category, Region), fill = Category))+
+  scale_fill_paletteer_d("nationalparkcolors::Acadia")+
+  labs(title = "Marimekko Chart",
+       caption = "Viz By: Bernard Kilonzo")+
+  theme(panel.background = element_blank(),
+        axis.title = element_text(family = "serif",face = "bold", size = 10, color = "gray30"),
+        axis.text = element_text(family = "serif", size = 9, color = "gray30"),
+        axis.ticks = element_line(linewidth = 0.1),
+        legend.title = element_text(family = "serif", face = "bold", size = 10, color = "gray30"),
+        legend.text = element_text(family = "serif", size = 9, color = "gray30"),
+        plot.title = element_text(family = "serif", face = "bold", size = 13, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray45"))
