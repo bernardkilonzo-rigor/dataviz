be3bba15e282e6d94aa48f9bef4c8a4fb1531f8e
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-28 16:05:42 +0300

    creating quadrants for coloring

--- code/Quadrant chart/Quadrant chart.R
+++ code/Quadrant chart/Quadrant chart.R
@@ -3,25 +3,22 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Quadrant 
 #load libraries
 library(tidyverse)
 
-#load dataset
+#load data set
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
-#quandrant boundaries
+#quadrant boundaries
 discount_mid <- mean(superstore$Discount)
 quantity_mid <- mean(superstore$Quantity)
 
-#Creating quandrant
-superstore%>%ggplot(aes(x = Discount, y = Quantity)) +
-  geom_point(size = 3, color = "steelblue") +
+#creating the quadrants for coloring
+superstore$quadrant<- with(superstore, ifelse(
+  superstore$Discount<discount_mid & superstore$Quantity<quantity_mid, "Q1",
+  ifelse(superstore$Discount<discount_mid & superstore$Quantity>quantity_mid, "Q2",
+         ifelse(superstore$Discount>discount_mid & superstore$Quantity<quantity_mid,"Q3","Q4")
+)))
+
+#Creating quadrant chart
+superstore%>%ggplot(aes(x = Discount, y = Quantity, color = quadrant)) +
+  geom_point(size = 3) +
   geom_vline(xintercept = discount_mid, linetype = "dashed", color = "gray") +
   geom_hline(yintercept = quantity_mid, linetype = "dashed", color = "gray")
-
-#coloring by quanadrant
-df$quadrant <- with(df, ifelse(x > x_mid & y > y_mid, "Q1",
-                               ifelse(x < x_mid & y > y_mid, "Q2",
-                                      ifelse(x < x_mid & y < y_mid, "Q3", "Q4"))))
-ggplot(df, aes(x = x, y = y, color = quadrant)) +
-  geom_point(size = 3) +
-  geom_vline(xintercept = x_mid, linetype = "dashed") +
-  geom_hline(yintercept = y_mid, linetype = "dashed") +
-  theme_minimal()
