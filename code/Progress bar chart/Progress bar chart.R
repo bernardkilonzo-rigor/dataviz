dd519266c0cbfc05d952bf25d6a8a6ea761de8a8
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-30 16:16:31 +0300

    Creating progress bar chart as a simple bar chart

--- code/Progress bar chart/Progress bar chart.R
+++ code/Progress bar chart/Progress bar chart.R
@@ -8,6 +8,21 @@ data<-data.frame(ID = 1:5,
                  Project_Task = c("Blog Post","Presentation","Workbook","Website","Video Blog"),
                  percent_complete =c(80,65,30,45,17))
 
+#creating progress bar chart (a simple bar chart)
+data%>%ggplot(aes(y = Project_Task, x = percent_complete))+
+  geom_bar(stat = "identity", fill = "steelblue", width = 0.4)+
+  geom_text(aes(label = paste0(percent_complete,"%")),size =2.5, vjust = 0.5, hjust = -0.1)+
+  labs(y = "Project Name", x = "Percent Completion",
+       title = "Progress Bar Chart", caption = "Viz By: Bernard Kilonzo")+
+  theme(panel.background = element_blank(),
+        axis.ticks = element_line(color = "gray30", linewidth = 0.1),
+        axis.line = element_line(color = "gray30", linewidth = 0.2),
+        axis.title = element_text(family = "serif", face = "bold", color = "gray25",size = 10),
+        axis.text = element_text(family = "serif", color = "gray25", size = 9),
+        plot.title = element_text(family = "serif", face = "bold", size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
+
+
 #Add a new column computing percent to completion
 data<-data%>%mutate(percent_to_complete = 100 -percent_complete)
 
