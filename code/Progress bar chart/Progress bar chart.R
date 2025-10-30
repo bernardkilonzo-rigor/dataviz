d2a7f84f1bdb838210ea6fa9a21323c2c8a37c1d
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-30 16:38:10 +0300

    Revising bar titles, and adding labs to the bar-in-bar chart

--- code/Progress bar chart/Progress bar chart.R
+++ code/Progress bar chart/Progress bar chart.R
@@ -12,8 +12,8 @@ data<-data.frame(ID = 1:5,
 data%>%ggplot(aes(y = Project_Task, x = percent_complete))+
   geom_bar(stat = "identity", fill = "steelblue", width = 0.4)+
   geom_text(aes(label = paste0(percent_complete,"%")),size =2.5, vjust = 0.5, hjust = -0.1)+
-  labs(y = "Project Name", x = "Percent Completion",
-       title = "Progress Bar Chart", caption = "Viz By: Bernard Kilonzo")+
+  labs(y = "Project Name", x = "Percent Complete",
+       title = "Project Completion Report", caption = "Viz By: Bernard Kilonzo")+
   theme(panel.background = element_blank(),
         axis.ticks = element_line(color = "gray30", linewidth = 0.1),
         axis.line = element_line(color = "gray30", linewidth = 0.2),
@@ -22,10 +22,12 @@ data%>%ggplot(aes(y = Project_Task, x = percent_complete))+
         plot.title = element_text(family = "serif", face = "bold", size = 12, color = "gray20"),
         plot.caption = element_text(family = "serif", face = "italic", size = 9, color = "gray35"))
 
-#Adding a new column computing 100% complete
+#adding a new column computing 100% complete
 data<-data%>%mutate(complete_project = 100)
 
 #creating progress bar chart (as a bar-in-bar chart)
 data%>%ggplot(aes(y = Project_Task))+
   geom_bar(aes(x = percent_complete), stat = "identity", width = 0.2, fill = "blue", alpha = 0.5)+
-  geom_bar(aes(x = complete_project), stat = "identity", width = 0.4, fill = "steelblue", alpha = 0.5)
+  geom_bar(aes(x = complete_project), stat = "identity", width = 0.4, fill = "steelblue", alpha = 0.5)+
+  labs(title = "Project Completion Report", caption = "Viz By: Bernard Kilonzo",
+       x = "Percent Complete", y = "Project Name")
