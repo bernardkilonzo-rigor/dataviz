b47de57a41e1c703ed84228192bb43c2c1b4ece9
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-10-31 15:29:42 +0300

    customizing chart using ggplot2 themes

--- code/Progress bar chart/Progress bar chart.R
+++ code/Progress bar chart/Progress bar chart.R
@@ -30,4 +30,11 @@ data%>%ggplot(aes(y = Project_Task))+
   geom_bar(aes(x = percent_complete), stat = "identity", width = 0.2, fill = "blue", alpha = 0.5)+
   geom_bar(aes(x = complete_project), stat = "identity", width = 0.4, fill = "steelblue", alpha = 0.5)+
   labs(title = "Project Completion Report", caption = "Viz By: Bernard Kilonzo",
-       x = "Percent Complete", y = "Project Name")
+       x = "Percent Complete", y = "Project Name")+
+  theme(panel.background = element_blank(),
+        axis.text = element_text(family = "serif", size = 9, color = "gray25"),
+        axis.title = element_text(family = "serif", face = "bold", size = 10, color = "gray25"),
+        axis.line = element_line(color = "gray25", linewidth = 0.1),
+        axis.ticks = element_line(linewidth = 0.1, color = "gray25"),
+        plot.title = element_text(family = "serif", face = "bold", size = 12, color = "gray20"),
+        plot.caption = element_text(family = "serif", face = "italic", size = 10, color = "gray45"))
