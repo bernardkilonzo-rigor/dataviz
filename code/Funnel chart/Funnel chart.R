a929872970953a08fd7c5f1dc33c59bb8502038b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-12-04 16:55:59 +0300

    creating funnel chart using plotly library

--- code/Funnel chart/Funnel chart.R
+++ code/Funnel chart/Funnel chart.R
@@ -42,3 +42,6 @@ advert_data<-advert_data[order(-advert_data$Value),]
 
 #converting the stages to factor
 advert_data$Stage<-factor(advert_data$Stage, levels = advert_data$Stage)
+
+#creating funnel chart with plotly library
+advert_data%>%plot_ly(y = ~Stage,x = ~Value,type = "funnel")
