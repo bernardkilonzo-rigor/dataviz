959d32c180de8c18562d1c436b13fc23aa6cd047
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-01-26 13:25:08 +0300

    Preparing sample dataset by computing the mean and standard error

--- code/Error bars/Error bars.R
+++ code/Error bars/Error bars.R
@@ -5,3 +5,13 @@ library(tidyverse)
 
 #loading data set
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
+#prepating sample dataset
+computed_data<-superstore%>%
+  group_by(Sub.Category)%>%
+  summarise(mean = mean(Sales), se =sd(Sales))
+
+#creating error bars for grouped data
+superstore%>%ggplot(aes(y = Sub.Category, x = Sales))+
+  geom_bar(stat = "summary", fun = mean)+
+  geom_errorbar(aes(xmin = mean-se, xmax = mean+se))
