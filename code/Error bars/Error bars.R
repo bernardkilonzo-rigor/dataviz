654db7971176c3703dce69e5bd85f592b649c751
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-01-26 13:11:33 +0300

    Loading the required libraries and dataset

---
+++ code/Error bars/Error bars.R
@@ -0,0 +1,7 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Error bars")
+
+#load libraries
+library(tidyverse)
+
+#loading data set
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
