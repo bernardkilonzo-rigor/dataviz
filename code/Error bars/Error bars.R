82c7cf054ebeb969a6df6933dee0f09aa3d4c284
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-01-27 13:32:47 +0300

    Saving the plot (error bars in a line plot)

--- code/Error bars/Error bars.R
+++ code/Error bars/Error bars.R
@@ -25,15 +25,18 @@ ggsave(plot = errbar, filename = "Error_bar.png",
        width = 8, height = 6, units = "in", dpi = 300)
 
 #creating error bars in line plots
-computed_data%>%ggplot(aes(x = Sub.Category, y = mean, group =1))+
+err_lne<-computed_data%>%ggplot(aes(x = Sub.Category, y = mean, group =1))+
   geom_line(color = "gray45", linewidth =0.9)+
   geom_point(size =3, color = "gray45")+
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), color = "tomato")+
   labs(title = "Error Bars in a Line Plot",x ="Sub Category", y = "Mean")+
   theme(panel.background = element_blank(),
         axis.line = element_line(color = "gray45", linewidth = 0.1),
-        axis.ticks = element_line(color = "gray45", linewidth = 0.1),
+        axis.ticks = element_line(color = "gray45", linewidth = 0.1),data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAABzElEQVR42u2Wy07CQBhGeRl3iF2Bz+DaRE0UNsYn0Z2vIE+hxLglMZq4IwYpV4G23JGrLS7GOTWsDEzbNMZFJ/l3c843l6bzx2L/aWhaaiehJTPxRPIysZe6DlIuKx24PIXuaqmDw+Ozm9u7e71QeHVKpbIIUrA4cOFU7vToJJOVkK3rFVEu/1SlUvVVaw4HLpxbd87R5HIPOlC1WhO1Wl3U6w23Go13T7WeD4sDF07cG4O5l2LxzQFC0my2RKvVFu22Icv0WIbLwOLAJZ2ruLZ/tXnH8qNgtUBITLMjOp2e6Hb7otcbeCrmwsDiwIUT99ZgJhqG5cL9/lAMh2MxGn2I8XjiqZgLA4sDF05lMKsEGAxGrmgymYnpdC5ms4WnYi4MLA5cOJXBHBGrBUQyny/FYvEplkvbUzEXBhYHLpzKYFbIUbFqBMhse+WrYGBx4MKpDObj4J5YMatH5DhfvgoGFgcunMpgvkyOiPta7zZIMCwOXDij4Cg4Cv6bYJ5F+V91wg42TUvxLMrHOp9/1MMOxrm1EaA9SacvspbVtcP6ZeLCqWz6aMxO0+fZp+cXdu4EfSRgceBSNnu/2lt5L4HbW1g/7W00ohHW+AYpkWmxvX/prAAAAABJRU5ErkJggg==
         axis.text.x = element_text(angle = 45, hjust = 1))
 
+#saving the plot
+ggsave(plot = err_lne, filename = "Error_Bars_Line_plot.png",
+       width = 8, height = 6, units = "in", dpi = 300)
 
 
