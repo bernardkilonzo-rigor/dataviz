99ae421848a41de482c533be5ac869957c5cae85
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-01-31 15:37:51 +0300

    creating a marginal density plot

--- code/Marginal density plot/Marginal density plot.R
+++ code/Marginal density plot/Marginal density plot.R
@@ -4,10 +4,13 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Marginal 
 library(tidyverse)
 library(ggExtra)
 
-#creating marginal density plot
-iris%>%ggplot()+
+#creating a scatter plot
+sc_plot<-iris%>%ggplot()+
   geom_point(aes(x = Sepal.Length, y = Sepal.Width, color = Species),
              alpha = 0.6, shape =16)+
   theme_minimal()+
   theme(legend.position = "bottom")+
   labs(x = "Sepal Length", y = "Sepal Width")
+
+#creating a marginal density plot
+ggMarginal(sc_plot, type = "density", groupColour = TRUE, groupFill = TRUE)
