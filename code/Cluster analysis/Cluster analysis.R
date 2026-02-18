8dd7f428ee2bd8583418c604e214bc0e6c25c454
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-02-18 13:20:40 +0300

    loading libraries and data set

---
+++ code/Cluster analysis/Cluster analysis.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Cluster analysis")
+
+#load library
+library(tidyverse)
+
+#load dataset
+superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
