127f1992f83b1da444a2870cf4f4c6c09d0f3d05
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-02-18 13:38:42 +0300

    preparing data and running k-means clustering

--- code/Cluster analysis/Cluster analysis.R
+++ code/Cluster analysis/Cluster analysis.R
@@ -3,6 +3,11 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Cluster a
 #load library
 library(tidyverse)
 
-#load dataset
+#load data set
 superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
+#extracting the numerical variables (sales & profit)
+data_num <-superstore[,c("Sales","Profit")]
+
+#Run K-means clustering
+km <- kmeans(data_num, centers = 4, nstart = 50)
