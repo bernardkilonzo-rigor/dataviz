b083996b1856a3b89d24a4b1e9055c1e14322358
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-02-19 12:48:00 +0300

    customizing plots theme

--- code/Cluster analysis/Cluster analysis.R
+++ code/Cluster analysis/Cluster analysis.R
@@ -1,5 +1,3 @@
-setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Cluster analysis")
-
 #load library
 library(tidyverse)
 
@@ -10,12 +8,18 @@ superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/da
 data_num <-superstore[,c("Sales","Profit")]
 
 #running K-means clustering
-km <- kmeans(data_num, centers = 4, nstart = 50)
+km <- kmeans(data_num, centers = 4, nstart = 25)
 
-#adding cluster labels to the table
+#adding cluster labels to the data set
 superstore$cluster <-factor(km$cluster)
 
 #visualizing the clusters
 superstore%>%ggplot(aes(x = Sales, y = Profit, color = cluster))+
-  geom_point(size =4)
+  geom_point(size =4, alpha =0.5)+
+  labs(title = "Cluster Analysis")+
+  theme(panel.background = element_blank(),
+        axis.title = element_text(family = "serif", face ="bold", color = "gray40"),
+        axis.text = element_text(family = "serif", color = "gray40"),
+        axis.line = element_line(linewidth = 0.1, color = "gray45"),
+        axis.ticks = element_line(linewidth = 0.1,color = "gray45"))
   
