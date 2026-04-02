32f6dd7910236a3491cf1b21599d7c7f712fef69
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-02 10:45:33 +0300

    Initial commit, creating tables in R

---
+++ code/Table/Table.R
@@ -0,0 +1,28 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Table")
+
+#load libraries
+
+
+
+#load data set
+
+
+
+#simple, clean, and flexible tables - knitr::kable()
+
+
+
+
+#beautiful, publication-quality tables - gt
+
+
+
+#interactive tables (search, sort, filter) - DT::datatable()
+
+
+#best for word/powerpoint reports - flextable
+
+
+
+#modern highly interactive tables - reactable
+
