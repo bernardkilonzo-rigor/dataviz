e35d2dc353e033e2a28c284aaddb7a901cceb266
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-06 14:54:39 +0300

    creating beautiful, publication-quality tables with gt.

--- code/Table/Table.R
+++ code/Table/Table.R
@@ -3,6 +3,7 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Table")
 #load libraries
 library(tidyverse)
 library(knitr)
+library(gt)
 
 #load data set
 superstore <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
@@ -16,8 +17,15 @@ x <- superstore%>%
 kable(x)
 
 #beautiful, publication-quality tables - gt
-
-
+x%>%gt()%>%
+  tab_style(
+    style = cell_borders(
+      sides = "bottom",
+      color = "black",
+      weight = px(2)
+    ),
+    locations = cells_column_labels()
+  )
 
 #interactive tables (search, sort, filter) - DT::datatable()
 
