0106c49f1ed4b7222ae763cbd63ce154a21d9e5a
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-08 14:37:48 +0300

    loading libraries and sample data, & computing sales summmary by State.

--- code/Circular packing/Circular packing.R
+++ code/Circular packing/Circular packing.R
@@ -1,8 +1,14 @@
 setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Circular packing")
 
 #load libraries
-
+library(tidyverse)
+library(packcircles)
 
 #load data set
+superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
+#computing total sales by state
+state_sales <- superstore %>%
+ group_by(State)%>%
+  summarise(sales =sum(Sales))
 
