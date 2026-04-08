4a8df83d7e1d4d777f7e7416426bc36942349382
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-08 15:24:46 +0300

    Preparing the circle packing layouts as well as polygon vertices

--- code/Circular packing/Circular packing.R
+++ code/Circular packing/Circular packing.R
@@ -12,3 +12,12 @@ state_sales <- superstore %>%
  group_by(State)%>%
   summarise(sales =sum(Sales))
 
+#generating a layout (assigning a center (x & y) and a radius proportional to the value)
+packing <- circleProgressiveLayout(state_sales$sales, sizetype = "area")
+
+#adding packing information to the initial data frame
+state_sales <- cbind(state_sales,packing)
+
+#converting circle packing layout into a data frame of polygon vertices that can be plottong with ggplot2
+dat.gg <- circleLayoutVertices(packing, npoints = 50)
+
