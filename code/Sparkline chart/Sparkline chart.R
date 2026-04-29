738cbf229960e572484ab8c23f2b173c3e70fb17
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-29 14:30:25 +0300

    Initial commit on creating a sparkline chart in R.

---
+++ code/Sparkline chart/Sparkline chart.R
@@ -0,0 +1,7 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Sparkline chart")
+#load libraries
+library(tidyverse)
+
+#load data set
+superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv"
+                      
