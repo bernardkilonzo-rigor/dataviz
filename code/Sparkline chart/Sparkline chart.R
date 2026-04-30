de8e95e5ee39bc734c82bd96e4372f3223e81858
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-30 12:44:52 +0300

    saving the sparkline chart as an html doc.

--- code/Sparkline chart/Sparkline chart.R
+++ code/Sparkline chart/Sparkline chart.R
@@ -3,6 +3,7 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\dataviz\\code\\Sparkline
 library(tidyverse)
 library(sparkline) #an html widget for creating sparklines
 library(zoo)
+library(htmlwidgets)
 
 #load data set
 superstore <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
@@ -14,11 +15,20 @@ sales_sum <- superstore%>%mutate(Order.Date = dmy(Order.Date))%>%
   summarise(sales = sum(Sales))
   
 #1. producing an inline sparkline that works inside Shiny, DT Tables, & HTML Reports
-sparkline(sales_sum$sales,
+sp<- sparkline(sales_sum$sales,
           type = "line",
           lineColor = "#2C7BE5",
           fillColor = "rgba(44,123,229,0.2)",
           spotColor = "#d9534f",
           minSpotColor = "#5cb85c",
           maxSpotColor = "#f0ad4e")                
-                      
+
+#saving the chart
+saveWidget(sp, "sparkline.html", selfcontained = TRUE)
+
+#2. create a sparkline chart using ggplot2
+
+
+
+
+#3. create a sparkline inside a table (DT + Sparkline)
